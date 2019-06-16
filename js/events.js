12563d6d615cec6c6ab3fe7f6d04bb9f18785a37
Kristy Reed <kristy.reed22@gmail.com>
2019-06-16 12:15:34 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -1,6 +1,29 @@
 //define functions here
 
+$('p').on('click', function getIt(){
+  alert("Hey!");
+  return;
+})
+
+$('img').on('load', function frameIt(){
+  
+})
+
+$("form").on('keydown', function pressIt(){
+  if(key.which == 71)
+  { alert('You have pressed the G key.');}
+})
+
+$("form").on("submit", function submitIt(){
+  if ($("input:first").val()==="correct")  
+    alert("Your form is going to be submitted now.");
+})
+
 $(document).ready(function(){
+  return getIt()
+  return frameIt()
+  return pressIt()
+  return submitIt()
 
 // call functions here
 
